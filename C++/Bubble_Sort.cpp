864f0bb283289821fd4307359469784af587ea99
Amjad-Khan-360 <2019cs45@student.uet.edu.pk>
2023-01-07 13:02:48 +0500

    Initial, searching Algos

--- C++/Bubble_Sort.cpp
+++ C++/Bubble_Sort.cpp
@@ -1,43 +1,36 @@
-#include <iostream>
-using namespace std;
+#include < iostream >
+  using namespace std;
 
-void bubbleSort(int *arr)
-{
-    for(int i = 1; i < 5; i++)
-    {
-        for(int j = 0; j < 5 - i; j++)
-        {
-            if(arr[j] > arr[j+1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
+void bubbleSort(int a[]) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < (5 - i - 1); j++) {
+      if (a[j] > a[j + 1]) {
+        int temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
     }
+  }
 }
 
-int main()
-{
-int arr[5] = {9, 3, 6, 5, 12};
+int main() {
+  int myarray[5];
+  int size;
+  cout << "Enter 5 integers in any order: " << endl;
+  for (int i = 0; i < 5; i++) {
+    cin >> myarray[i];
+  }
+  cout << "Before Sorting" << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << myarray[i] << " ";
+  }
 
-cout << "Unsorted:\n";    
-    for(int i = 0; i < 5; i++)
-    {
-        cout << arr[i];
-        if(i < 4)
-        cout << ", ";
-    }
-    cout << endl;
+  bubbleSort(myarray); // sorting
 
-bubbleSort(arr);
+  cout << endl << "After Sorting" << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << myarray[i] << " ";
+  }
 
-cout << "Sorted:\n";    
-    for(int i = 0; i < 5; i++)
-    {
-        cout << arr[i];
-        if(i < 4)
-        cout << ", ";
-    }
-    cout << endl;
+  return 0;
 }
